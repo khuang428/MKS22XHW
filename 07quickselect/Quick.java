a0898937a7afb433cff98fd5d7af5cbdd26f7550
Karen Huang <khuang428@gmail.com>
2016-03-09 21:49:40 -0500

    Added quickSort and name functions

--- 07quickselect/Quick.java
+++ 07quickselect/Quick.java
@@ -1,32 +1,35 @@
 public class Quick{
+    
+    private static void swap(int[]data,int a, int b){
+	int temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
+    }
+
     private static int partition(int[]data,int left, int right){
-	int ctr = 0;
-	int[]ary = new int[data.length];
-	int part = left +(int)(Math.random() * (right - left));
-	for(int i = 0;i < data.length;i++){
-	    if(data[i] < data[part]){
-		ary[ctr] = data[i];
-		ctr++;
+	int part = left +(int)(Math.random() * (right - left + 1));
+	int start = left + 1, end = right;
+	while(start < end){
+	    if(data[start] < data[left]){
+		swap(data,start,end);
+		end--;
+	    }else{
+		start++;
 	    }
 	}
-	int index = ctr;
-	for(int i = 0;i < data.length;i++){
-	    if(data[i] == data[part]){
-		ary[ctr] = data[i];
-		ctr++;
-	    }
+	if(data[start] > data[left]){
+	    swap(data,left,start - 1);
+	    return start - 1;
+	}else{
+	    swap(data,left,start);
+	    return start;
 	}
-	for(int i = 0;i < data.length;i++){
-	    if(data[i] > data[part] ){
-		ary[ctr] = data[i];
-		ctr++;
-	    }
-	}
-	data = ary;
-	return index;
     }
 
     private static int quickselect(int[]data, int k, int left, int right){
+	if(left == right){
+	    return data[left];
+	}
 	int index = partition(data,left,right);
 	if(index == k){
 	    return data[k];
@@ -39,5 +42,21 @@ public class Quick{
 
     public static int quickselect(int[]data, int k){
 	return quickselect(data,k,0,data.length - 1);
+    }
+
+    public static String name(){
+	return "7,Huang,Karen";
+    }
+
+    private static void quickSort(int[]data,int left,int right){
+	if(left != right){
+	    int index = partition(data,left,right);
+	    quickSort(data,index + 1,right);
+	    quickSort(data,left,index - 1);
+	}
+    }
+
+    public static void quickSort(int[]data){
+	quickSort(data,0,data.length - 1);
     }
 }
