f6485b17f1f3927e855b04e0dd07a357dca886dd
Karen Huang <khuang428@gmail.com>
2016-04-10 16:18:16 -0400

    Made FrontierStack

---
+++ 11Frontier/FrontierStack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FrontierStack<T> implements Frontier<T>{
+    private Stack<T> s = new Stack<T>();
+
+    public void add(T element){
+	s.push(element);
+    }
+
+    public T next(){
+	return s.peek();
+    }
+
+    public boolean hasNext(){
+	try{
+	    s.peek();
+	}catch(EmptyStackException e){
+	    return false;
+	}
+	return true;
+    }
+}
