9c83bc1a6d5ef21215bfea352f5cdaf6ac55de97
Karen Huang <khuang428@gmail.com>
2016-05-11 00:34:24 -0400

    Finished running median

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,21 +1,31 @@
 import java.util.*;
+
 public class RunningMedian{
-    private MyHeap min,max;
+    private MyHeap<Integer> min,max;
     private int minSize,maxSize;
 
     public RunningMedian(){
-	min = new MyHeap(false);
-	max = new MyHeap();
+	min = new MyHeap<Integer>(false);
+	max = new MyHeap<Integer>();
     }
 
     public double getMedian(){
 	if(minSize == 0 && maxSize == 0){
 	    throw new NoSuchElementException();
 	}
-	return 0;
+	if(minSize == 0 || maxSize > minSize){
+	    return (double)max.peek();
+	}else if(minSize > maxSize){
+	    return(double)min.peek();
+	}
+	return (double)((min.peek() + max.peek()) / 2);
     }
 
     public void add(Integer x){
+	if(minSize == 0 && maxSize == 0){
+	    max.add(x);
+	    maxSize++;
+	}
 	if(x < getMedian()){
 	    min.add(x);
 	    minSize++;
@@ -24,7 +34,14 @@ public class RunningMedian{
 	    maxSize++;
 	}
 	if(minSize + 1 > maxSize){
-	    
+	    max.add(min.delete());
+	    maxSize++;
+	    minSize--;
+	}
+	if(maxSize + 1 > minSize){
+	    min.add(max.delete());
+	    minSize++;
+	    maxSize--;
 	}
     }
 }
